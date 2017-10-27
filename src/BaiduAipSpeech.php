31e6b7309527fdd574397a5ee3a4ca9b0ec09946
Tommy Lau <tommy@gen-new.com>
2017-10-28 05:30:26 +0800

    Finish base TTS function

--- src/BaiduAipSpeech.php
+++ src/BaiduAipSpeech.php
@@ -27,4 +27,29 @@ class BaiduAipSpeech
             return '';
         }
     }
+
+    public static function synthesis($text, $lang = 'zh', $ctp = 1)
+    {
+        $aipSpeech = new AipSpeech(
+            config('baidu.aip.app_id'),
+            config('baidu.aip.api_key'),
+            config('baidu.aip.secret_key')
+        );
+
+        $options = [
+            // 0为女声，1为男声，3为情感合成-度逍遥，4为情感合成-度丫丫，默认为普通女
+            'per' => 0,
+        ];
+
+        $response = $aipSpeech->synthesis($text, $lang, $ctp, $options);
+
+        if (!is_array($response)) {
+            $filename = 'voice/' . uniqid(mt_rand(), true) . '.mp3';
+            file_put_contents($filename, $response);
+            return $filename;
+        } else {
+            return $response;
+        }
+    }
+
 }
